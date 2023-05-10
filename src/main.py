ca82dd6d6685ccc338a9b7484e506da5c7791e2f
Gaetano <olimpusdrs@gmail.com>
2023-05-10 19:23:00 +0200

    Added multi-language support with form

--- src/main.py
+++ src/main.py
@@ -13,36 +13,47 @@ api = Api()
 #llm default OpenAPI
 llm = api.llm
 
-#streamlit view component
-st.title('Sentiment Analysis')
-text_review = st.text_area('Write me a review in Italian') 
 
-#1 prompt template
-template = """
-Please act as a machine learning model trained for perform a supervised learning task, 
-for extract the sentiment of a review in Italian Language.
+#streamlit view components
+with st.form("my_form"):
+    st.title('Sentiment Analysis')
+    text_review = st.text_area('Write me a review') 
 
-Give your answer writing a Json evaluating the sentiment field between the dollar sign, the value must be printed without dollar sign.
-The value of sentiment must be "positive"  or "negative", otherwise if the text is not valuable write "null".
+    option = st.selectbox(
+    'Select the language to evaluate:',
+    ('Italian', 'Spanish', 'English'))
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        
+        #1 prompt template
+        template = """
+        Please act as a machine learning model trained for perform a supervised learning task, 
+        for extract the sentiment of a review in '{option}' Language.
 
-Example:
+        Give your answer writing a Json evaluating the sentiment field between the dollar sign, the value must be printed without dollar sign.
+        The value of sentiment must be "positive"  or "negative", otherwise if the text is not valuable write "null".
 
-field 1 named :
-text_review with value: {text_review}
-field 2 named :
-sentiment with value: $sentiment$
+        Example:
 
-Review text: '''{text_review}'''
+        field 1 named :
+        text_review with value: {text_review}
+        field 2 named :
+        sentiment with value: $sentiment$
+        Field 3 named : 
+        language with value: {option}
+        Review text: '''{text_review}'''
 
-"""
-prompt = PromptTemplate(template=template, input_variables=["text_review"])
+        """
 
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+        prompt = PromptTemplate(template=template, input_variables=["text_review","option"])
 
-question = text_review
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-if prompt:
-    response = llm_chain.run(question)
-    #json printed
-    print(response)
-    st.text(response) 
+        question = text_review
+
+        if prompt:
+            response = llm_chain.run({"text_review": question, "option": option})
+            #json printed
+            print(response)
+            st.text(response) 
+
